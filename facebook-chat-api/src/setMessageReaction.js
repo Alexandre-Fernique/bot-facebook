7cd7a57227e1ba1e4ef2cfac59938fbf541e1996
Alexandre-Fernique <alexandre.fernique@etu.umontpellier.fr>
2021-07-25 14:01:26 +0200

    update

--- facebook-chat-api/src/setMessageReaction.js
+++ facebook-chat-api/src/setMessageReaction.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var utils = require("facebook-chat-api/utils");
+var utils = require("../utils");
 var log = require("npmlog");
 
 module.exports = function(defaultFuncs, api, ctx) {
