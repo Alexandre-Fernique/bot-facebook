7db8ef2f92a6af9bef6f7639812a62482a9bc070
Alexandre-Fernique <alexandre.fernique@etu.umontpellier.fr>
2021-07-27 01:27:35 +0200

    Ajout du reamde et chanson

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const login = require("facebook-chat-api");
 
 // Create simple echo bot
 const fs = require("fs");
-
+var hello=["Bonjour","Bonsoir","Salut","Hello","Yo","Coucou","bonjour","bonsoir","salut","hello","yo","coucou"]
 login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
     if(err) return console.error(err);
     // Here you can use the api
@@ -21,9 +21,30 @@ login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, ap
             if(err) return console.error(err);
             else {
                 if(message.body)
-                console.log("Resaction send to "+ message.body);
+                console.log("Reaction send to "+ message.body);
             }
         })
+        if(message.body!=undefined){
+            for(let val of hello){
+
+                if(message.body.search(val)!==-1 ) {
+                    console.log("Envoi coucou");
+                    var msg = {attachment: fs.createReadStream(__dirname + '/assets/Coucou2.mp3')}
+                    api.sendMessage(msg, message.threadID);
+                }
+            }
+            if(message.body.search("Chante")!=-1 ||message.body.search("chante")!=-1){
+                var msg={
+                    body: "Je chante pour toi",
+                    attachment:fs.createReadStream(__dirname + '/assets/brocolis.mp3')
+                }
+                console.log("Envoi chante");
+                api.sendMessage(msg, message.threadID);
+
+            }
+
+        }
+
 
     });
 });
---
+++ readme.md
@@ -0,0 +1,10 @@
+#Help Bot
+___
+* **Email** : auberginebot5@gmail.com
+
+* **Mot de passe** : &botaubergine
+### En cas de problème appstate.json
+Téléchargez depuis Brave avec J2TEAM cookie les cookies de connexions de Facebook dans appstate.json cookies:[...]
+
+Remplace ensuite toute les occurences de "name" par "key"
+
