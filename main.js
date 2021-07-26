38113b2ad7d0f52139dccdeaa1cc2de340db4b87
Alexandre-Fernique <alexandre.fernique@etu.umontpellier.fr>
2021-07-26 20:21:34 +0200

    Update problem conexion

--- main.js
+++ main.js
@@ -8,11 +8,10 @@ login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, ap
     if(err) return console.error(err);
     // Here you can use the api
     api.listenMqtt((err, message) => {
-        console.log(message.mentions);
-        console.log(typeof message.mentions);
         if(message.mentions!=undefined)
         {
             if(Object.keys(message.mentions).indexOf("100064795671628")!=-1){
+                console.log(message.mentions);
                 api.sendMessage("TKT - Des Brocolis\n https://www.youtube.com/watch?v=xVL29dVdLN4", message.threadID);
             }
         }
@@ -20,6 +19,10 @@ login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, ap
 
         api.setMessageReaction(decodeURIComponent("🍆"),message.messageID,(err)=>{
             if(err) return console.error(err);
+            else {
+                if(message.body)
+                console.log("Resaction send to "+ message.body);
+            }
         })
 
     });
